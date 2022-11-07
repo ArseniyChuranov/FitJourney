9a5fc483d65c933e63b948b6f688de81510f0344
Arseniy <Arseniy.churanov@gmail.com>
2022-11-07 10:15:56 -0800

    version 1

---
+++ Fit_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_JourneyApp.swift
+//  Fit Journey
+//
+//  Created by Arseniy Churanov on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Fit_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
